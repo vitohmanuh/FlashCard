5e0e06dab0b45f26914a6bb6a7fd095a0e200f20
vitohmanuh <emanuelle.vito@escola.pr.gov.br>
2024-11-06 11:55:12 -0300

    Add files via upload

---
+++ Nova pasta/app.js
@@ -0,0 +1,7 @@
+function criaCartao (categoria, pergunta, resposta) {
+ let container = document.getElementById ('container')
+ let cartao = document.createElement ( ´article ´)
+ cartao.className = ´ cartao ´
+
+ cartao.innerHTML
+}
